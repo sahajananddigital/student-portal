38c7fa9c449aa16d742009a7580e80b2f394b4b6
Dishant <devsangani168@gmail.com>
2026-01-21 11:41:34 +0530

    fix origin

--- Backend/index.php
+++ Backend/index.php
@@ -1,5 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: * ");
+header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
 header("Access-Control-Allow-Methods: POST, PUT, GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
